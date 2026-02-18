2f5bf49a439ec78f06eb61bd98e9d4c381ed8807
StruggleMick <s284250@studenti.polito.it>
2026-02-18 17:47:02 +0100

    update

--- src/TestPage.tsx
+++
@@ -1,7 +0,0 @@
-// TestPage.tsx
-import React from 'react';
-function TestPage() {
-    console.log("TestPage è stata renderizzata!");
-    return <h1>Sono la pagina di test!</h1>;
-}
-export default TestPage;
